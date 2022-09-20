a1d78054df6f09e01a9febeacecdadaaa4193726
hyejeong99 <59854960+hyejeong99@users.noreply.github.com>
2022-09-21 01:42:16 +0900

    c#으로 PDF 저장 및 이메일 전송

---
+++ ExportToPdf/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ExportToPdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_savePDF = new System.Windows.Forms.Button();
+            this.btn_printPDF = new System.Windows.Forms.Button();
+            this.btn_sendEmail = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(763, 320);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_savePDF
+            // 
+            this.btn_savePDF.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold);
+            this.btn_savePDF.Location = new System.Drawing.Point(190, 382);
+            this.btn_savePDF.Name = "btn_savePDF";
+            this.btn_savePDF.Size = new System.Drawing.Size(131, 33);
+            this.btn_savePDF.TabIndex = 1;
+            this.btn_savePDF.Text = "PDF로 저장";
+            this.btn_savePDF.UseVisualStyleBackColor = true;
+            this.btn_savePDF.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_printPDF
+            // 
+            this.btn_printPDF.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold);
+            this.btn_printPDF.Location = new System.Drawing.Point(327, 382);
+            this.btn_printPDF.Name = "btn_printPDF";
+            this.btn_printPDF.Size = new System.Drawing.Size(131, 33);
+            this.btn_printPDF.TabIndex = 2;
+            this.btn_printPDF.Text = "PDF로 인쇄";
+            this.btn_printPDF.UseVisualStyleBackColor = true;
+            // 
+            // btn_sendEmail
+            // 
+            this.btn_sendEmail.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold);
+            this.btn_sendEmail.Location = new System.Drawing.Point(464, 382);
+            this.btn_sendEmail.Name = "btn_sendEmail";
+            this.btn_sendEmail.Size = new System.Drawing.Size(131, 33);
+            this.btn_sendEmail.TabIndex = 3;
+            this.btn_sendEmail.Text = "이메일 전송";
+            this.btn_sendEmail.UseVisualStyleBackColor = true;
+            this.btn_sendEmail.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_sendEmail);
+            this.Controls.Add(this.btn_printPDF);
+            this.Controls.Add(this.btn_savePDF);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_savePDF;
+        private System.Windows.Forms.Button btn_printPDF;
+        private System.Windows.Forms.Button btn_sendEmail;
+    }
+}
+
---
+++ ExportToPdf/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.Net.Mail;
+
+namespace ExportToPdf
+{
+    public partial class Form1 : Form
+    {
+        public string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\82104\source\repos\ExportToPdf\ExportToPdf\Database1.mdf;Integrated Security=True;Connect Timeout=30";
+        public Form1()
+        {
+            InitializeComponent();
+
+            //데이터베이스 값 가져와서 보여주기
+            try
+            {
+                /*string query = @"SELECT * FROM tblEmployee";
+                DataSet ds = new System.Data.DataSet();
+                OracleDBManager.Instance.ExecuteDsQuery(ds, query);*/
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = @"SELECT * FROM tblEmployee";
+                SqlDataReader sdr = cmd.ExecuteReader();
+
+                /*if (ds == null || ds.Tables.Count == 0 || ds.Tables[0].Rows.Count == 0)
+                    return;
+                dataGridView1.DataSource = ds.Tables[0];*/
+
+                while (sdr.Read() == true)
+                {
+                    dataGridView1.DataSource = sdr["EmployeeId"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon;
+            string conString = null;
+            string sqlQuery = null;
+
+            conString = "Data Source=.;Initial Catalog=DemoTest;Integrated Security=SSPI;";
+            //conString= @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\82104\source\repos\lightP03\lightP03\Database1.mdf;Integrated Security=True;Connect Timeout=30";
+            //sqlCon = new SqlConnection(conString);
+            sqlCon = new SqlConnection(connectionString);
+
+            sqlCon.Open();
+            sqlQuery = "SELECT * FROM tblEmployee";
+            SqlDataAdapter dscmd = new SqlDataAdapter(sqlQuery, sqlCon);
+            DataTable dtData = new DataTable();
+            dscmd.Fill(dtData);
+            dataGridView1.DataSource = dtData;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "PDF (*.pdf)|*.pdf";
+                sfd.FileName = "Output.pdf";
+                bool fileError = false;
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(sfd.FileName))
+                    {
+                        try
+                        {
+                            File.Delete(sfd.FileName);
+                        }
+                        catch (IOException ex)
+                        {
+                            fileError = true;
+                            MessageBox.Show("It wasn't possible to write the data to the disk." + ex.Message);
+                        }
+                    }
+                    if (!fileError)
+                    {
+                        try
+                        {
+                            PdfPTable pdfTable = new PdfPTable(dataGridView1.Columns.Count);
+                            pdfTable.DefaultCell.Padding = 3;
+                            pdfTable.WidthPercentage = 100;
+                            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+
+                            foreach (DataGridViewColumn column in dataGridView1.Columns)
+                            {
+                                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                                pdfTable.AddCell(cell);
+                            }
+
+                            foreach (DataGridViewRow row in dataGridView1.Rows)
+                            {
+                                foreach (DataGridViewCell cell in row.Cells)
+                                {
+                                    pdfTable.AddCell(cell.Value.ToString());
+                                }
+                            }
+
+                            using (FileStream stream = new FileStream(sfd.FileName, FileMode.Create))
+                            {
+                                Document pdfDoc = new Document(PageSize.A4, 10f, 20f, 20f, 10f);
+                                PdfWriter.GetInstance(pdfDoc, stream);
+                                pdfDoc.Open();
+                                pdfDoc.Add(pdfTable);
+                                pdfDoc.Close();
+                                stream.Close();
+                            }
+
+                            MessageBox.Show("Data Exported Successfully !!!", "Info");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error :" + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No Record To Export !!!", "Info");
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MailMessage mail = new MailMessage();
+            try
+            {   // 보내는 사람 메일, 이름, 인코딩(UTF-8)      
+                mail.From = new MailAddress("gg2060gg@gmail.com", "명월일지", System.Text.Encoding.UTF8);
+                // 받는 사람 메일      
+                mail.To.Add("gg2060gg@gmail.com");
+                // 참조 사람 메일      
+                mail.CC.Add("gg2060gg@gmail.com");
+                // 비공개 참조 사람 메일      
+                mail.Bcc.Add("gg2060gg@gmail.com");
+                // 메일 제목      
+                mail.Subject = "PDF 파일";
+                // 본문 내용      
+                mail.Body = "<html><body>hello wrold</body></html>";
+                // 본문 내용 포멧의 타입 (true의 경우 Html 포멧으로)      
+                mail.IsBodyHtml = true;
+
+                // 메일 제목과 본문의 인코딩 타입(UTF-8)      
+                mail.SubjectEncoding = System.Text.Encoding.UTF8;
+                mail.BodyEncoding = System.Text.Encoding.UTF8;
+                // 첨부 파일 (Stream과 파일 이름)      
+                mail.Attachments.Add(new Attachment(new FileStream(@"C:\Users\82104\source\repos\ExportToPdf\test1.pdf", FileMode.Open, FileAccess.Read), "test1.pdf"));
+                //mail.Attachments.Add(new Attachment(new FileStream(@"D:\test2.zip", FileMode.Open, FileAccess.Read), "test2.zip"));
+
+                // smtp 서버 주소      
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                // smtp 포트      
+                SmtpServer.Port = 587;
+                // smtp 인증      
+                SmtpServer.Credentials = new System.Net.NetworkCredential("gg2060gg", "vwsfdgymtyfqqeuu");
+                // SSL 사용 여부      
+                SmtpServer.EnableSsl = true;
+                // 발송      
+                SmtpServer.Send(mail);
+            }
+            finally
+            {
+                // 첨부 파일 Stream 닫기      
+                foreach (var attach in mail.Attachments)
+                {
+                    attach.ContentStream.Close();
+                }
+            }
+        }
+    }
+}
